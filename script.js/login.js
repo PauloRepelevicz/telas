3ad0c252a6db3882344723cd2d9c432d5c2354ff
PauloRepelevicz <159267295+PauloRepelevicz@users.noreply.github.com>
2025-08-06 10:26:50 -0300

    Delete script.js directory

--- script.js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Espera 100ms para garantir que o DOM está pronto
-    setTimeout(function() {
-        document.body.classList.add('animated');
-    }, 100);
-});
