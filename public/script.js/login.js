d02f2a37f53150fe026f14eef629dbd4fc65b681
LeonardoMiguel04 <159266494+LeonardoMiguel04@users.noreply.github.com>
2025-08-11 10:53:10 -0300

    Add files via upload

---
+++ public/script.js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Espera 100ms para garantir que o DOM está pronto
+    setTimeout(function() {
+        document.body.classList.add('animated');
+    }, 100);
+});
