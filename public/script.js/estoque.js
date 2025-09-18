fb00eb257c63e995d23ca048a7e8de7ae1b670fd
LeonardoMiguel04 <159266494+LeonardoMiguel04@users.noreply.github.com>
2025-09-17 23:15:16 -0300

    Update estoque.js

--- public/script.js/estoque.js
+++ public/script.js/estoque.js
@@ -1,140 +1,162 @@
-async function cadastrarProduto(event) {
-    event.preventDefault();
-    const produto = {
-        nome: document.getElementById("nomeprod").value,
-        codigo: document.getElementById("codprod").value,
-        venda: document.getElementById("precoprod").value,
-        descricao: document.getElementById("descricaoprod").value,
-        categoria: document.getElementById("categoriaprod").value,
-        quantidade_estoque: document.getElementById("quantidadeprod").value,
-        unidade_medida: document.getElementById("unidademedidaprod").value,
-        estoque_emergencia: document.getElementById("emergenciaprod").value,
-        fornecedor: document.getElementById("fornecedoresSelecionados").value,
-    };
-    try {
-        const response = await fetch("/produto", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(produto),
-        });
-        alert("Glória a Deus!");
-        const result = await response.json();
-        if (response.ok) {
-            alert("Produto cadastrado com sucesso!");
-            document.getElementById("produtoForm").reset();
-        } else {
-            alert(`Erro: ${result.message}`);
-        }
-        alert("amem Jesus");
-    } catch (err) {
-        console.error("Erro na solicitação:", err);
-        alert("Erro ao cadastrar produto.estoque.js");
-    }
+async function cadastrarForn(event) {
+  event.preventDefault();
+  const fornecedor = {
+      nome: document.getElementById("nomeforn").value,
+      cnpj: document.getElementById("cnpjforn").value,
+      telefone: document.getElementById("telefoneforn").value,
+      email: document.getElementById("emailforn").value,
+      logradouro: document.getElementById("enderecoforn").value,
+      numero: document.getElementById("numeroforn").value,
+      bairro: document.getElementById("bairroforn").value,
+      cidade: document.getElementById("cidadeforn").value,
+      estado: document.getElementById("estadoforn").value,
+      cep: document.getElementById("cepforn").value,
+      complemento: document.getElementById("complementoforn").value,
+      observacoes: document.getElementById("observacoesforn").value
+  };
+  try {
+      const response = await fetch("/fornecedor", {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(fornecedor),
+      });
+      const result = await response.json();
+      if (response.ok) {
+          alert("Fornecedor cadastrado com sucesso!");
+          document.getElementById("FornecedorForm").reset();
+      } else {
+          alert(`Erro: ${result.message}`);
+      }
+  } catch (err) {
+      console.error("Erro na solicitação:", err);
+      //alert("Erro ao cadastrar fornecedor.fornecedor.js");
+  }
 }
 
-function buscarFornecedores() {
-    fetch("/fornecedor")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Erro ao buscar fornecedores");
-            }
-            return response.json();
-        })
-        .then((servicos) => {
-            const select = document.getElementById("fornecedoresSelecionados");
-            servicos.forEach((produto) => {
-                const option = document.createElement("option");
-                option.value = produto.forn_id; // Usa o id como valor
-                option.textContent = produto.forn_nome; // Nome do serviço exibido
-                select.appendChild(option);
-            });
-        })
-        .catch((error) => {
-            console.error("Erro ao carregar os serviços:", error);
-        });
+// ----------------- LISTAR FORNECEDOR -----------------
+async function listarFornecedor() {
+  const cnpj = document.getElementById("cnpjforn").value.trim();
+  let url = "/fornecedor";
+
+  if (cnpj) url += `?cnpj=${cnpj}`;
+
+  try {
+      const response = await fetch(url);
+      const fornecedor = await response.json();
+
+      const tabela = document.getElementById("tabela-fornecedores");
+      tabela.innerHTML = "";
+
+      if (fornecedor.length === 0) {
+          tabela.innerHTML =
+              '<tr><td colspan="6">Nenhum fornecedor encontrado.</td></tr>';
+      } else {
+          fornecedor.forEach((fornecedor) => {
+              const linha = document.createElement("tr");
+              linha.innerHTML = `
+                  <td>${fornecedor.forn_id}</td>
+                  <td>${fornecedor.forn_nome}</td>
+                  <td>${fornecedor.forn_cnpj}</td>
+                  <td>${fornecedor.forn_email}</td>
+                  <td>${formatarTelefone(fornecedor.forn_telefone || "")}</td>
+                  <td>${formatarCEP(fornecedor.forn_cep || "")}</td>
+              `;
+              tabela.appendChild(linha);
+          });
+      }
+  } catch (error) {
+      console.error("Erro ao listar fornecedores:", error);
+  }
 }
 
-// ----------------- LISTAR PRODUTOS ----------------- //
-async function listarProdutos() {
-    try {
-        const response = await fetch("/produto");
+// ----------------- ATUALIZAR FORNECEDOR -----------------
+async function atualizarFornecedor() {
+    alert("dj marciano o mais brabo(estou no fornecedor.js)");
+    const cnpj = document.getElementById("cnpjforn").value;
 
-        if (response.ok) {
-            const produtos = await response.json();
+    const fornecedorAtualizado = { 
+      nome: document.getElementById("nomeforn").value,
+      telefone: document.getElementById("telefoneforn").value,
+      email: document.getElementById("emailforn").value,
+      logradouro: document.getElementById("enderecoforn").value,
+      numero: document.getElementById("numeroforn").value,
+      bairro: document.getElementById("bairroforn").value,
+      cidade: document.getElementById("cidadeforn").value,
+      estado: document.getElementById("estadoforn").value,
+      cep: document.getElementById("cepforn").value,
+      complemento: document.getElementById("complementoforn").value,
+      observacoes: document.getElementById("observacoesforn").value
+  };
 
-            const tabela = document.getElementById("table-clientes");
-            tabela.innerHTML = ""; // Limpa a tabela antes de preencher
+  try {
+      const response = await fetch(`/fornecedor/cnpj/${cnpj}`, {
+          method: "PUT",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify(fornecedorAtualizado),
+      });
 
-            produtos.forEach((produto) => {
-                const linha = document.createElement("tr");
-                linha.innerHTML = `
-                    <td>${produto.prod_id}</td>
-                    <td>${produto.prod_nome}</td>
-                    <td>${produto.prod_codigo}</td>
-                    <td>${produto.prod_preco_venda}</td>
-                    <td>${produto.cat_nome}</td>
-                    <td>${produto.prod_quantidade_estoque}</td>
-                    <td>${produto.forn_nome}</td>
-                `;
-                tabela.appendChild(linha);
-            });
-        } else {
-            alert("Erro ao listar produtos.1111");
-        }
-    } catch (error) {
-        console.error("Erro ao listar produtos:222222222", error);
-        alert("Erro ao listar produtos3333333333333.");
-    }
+      if (response.ok) {
+          alert("Fornecedor atualizado com sucesso!");
+      } else {
+          const errorMessage = await response.text();
+          alert("Erro ao atualizar fornecedor: " + errorMessage);
+      }
+  } catch (error) {
+      console.error("Erro ao atualizar fornecedor:", error);
+      alert("Erro ao atualizar fornecedor.1111");
+  }
 }
 
-// ----------------- LIMPAR CAMPO -----------------
-
-async function limpaform() {
-    document.getElementById("nomeprod").value = "";
-    document.getElementById("codprod").value = "";
-    document.getElementById("precoprod").value = "";
-    document.getElementById("descricaoprod").value = "";
-    document.getElementById("categoriaprod").value = "";
-    document.getElementById("quantidadeprod").value = "";
-    document.getElementById("emergenciaprod").value = "";
-    document.getElementById("unidademedidaprod").value = "";
-    document.getElementById("fornecedoresSelecionados").value = "";
+async function limpaForn() {
+  document.getElementById("nomeforn").value = "";
+  document.getElementById("cnpjforn").value = "";
+  document.getElementById("emailforn").value = "";
+  document.getElementById("telefoneforn").value = "";
+  document.getElementById("enderecoforn").value = "";
 }
 
-// ----------------- ATUALIZAR PRODUTO -----------------
-async function atualizarProduto() {
-    const codigo = document.getElementById("codprod").value;
+// ----------------- FORMATAÇÕES -----------------
 
-    const produtoAtualizado = {
-        nome: document.getElementById("nomeprod").value,
-        venda: document.getElementById("precoprod").value,
-        descricao: document.getElementById("descricaoprod").value,
-        categoria: document.getElementById("categoriaprod").value,
-        quantidade_estoque: document.getElementById("quantidadeprod").value,
-        unidade_medida: document.getElementById("unidademedidaprod").value,
-        estoque_emergencia: document.getElementById("emergenciaprod").value,
-        fornecedor: document.getElementById("fornecedoresSelecionados").value,
-    };
-
-    try {
-        const response = await fetch(`/produto/codigo/${codigo}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(produtoAtualizado),
-        });
-
-        if (response.ok) {
-            alert("Produto atualizado com sucesso!");
-        } else {
-            const errorMessage = await response.text();
-            alert("Erro ao atualizar Produto: " + errorMessage);
-        }
-    } catch (error) {
-        console.error("Erro ao atualizar Produto:", error);
-        alert("Erro ao atualizar Produto.11111111");
-    }
+// CEP
+function formatarCEP(cepforn) {
+  return cepforn
+      .replace(/\D/g, "")
+      .replace(/(\d{5})(\d)/, "$1-$2")
+      .slice(0, 9);
 }
+document.getElementById("cepforn").addEventListener("input", async (e) => {
+  e.target.value = formatarCEP(e.target.value);
+
+  const cep = e.target.value.replace(/\D/g, "");
+  if (cep.length === 8) {
+      try {
+          const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+          const data = await response.json();
+          if (!data.erro) {
+              document.getElementById("enderecoforn").value = data.logradouro || "";
+              document.getElementById("bairroforn").value = data.bairro || "";
+              document.getElementById("cidadeforn").value = data.localidade || "";
+              document.getElementById("estadoforn").value = data.uf || "";
+          }
+      } catch (err) {
+          console.error("Erro ao buscar CEP:", err);
+      }
+  }
+});
+
+// TELEFONE
+function formatarTelefone(tel) {
+  tel = tel.replace(/\D/g, "");
+  if (tel.length <= 10) {
+      return tel.replace(/(\d{2})(\d{4})(\d{0,4})/, "($1) $2-$3");
+  } else {
+      return tel.replace(/(\d{2})(\d{5})(\d{0,4})/, "($1) $2-$3");
+  }
+}
+document.getElementById("telefoneforn").addEventListener("input", (e) => {
+  e.target.value = formatarTelefone(e.target.value);
+});
